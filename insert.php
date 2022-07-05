3f1b7c60dc1bbe19511b2a5d500a6f3ece4d98f8
tirtheshs47 <107477840+tirtheshs47@users.noreply.github.com>
2022-07-05 07:28:35 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -6,14 +6,20 @@
         "PWD" => "Admin@123" 
     );
     //Establishes the connection
+
+
+    //Establishes the connection
+
     $conn = sqlsrv_connect($serverName, $connectionOptions);
+
     $tsql= "INSERT INTO employeeinfo (fname, lname) VALUES('$_POST[fname]','$_POST[lname]')";
+
     $getResults= sqlsrv_query($conn, $tsql);
+
     echo ("Data entered successfully!" . PHP_EOL);
+
     if ($getResults == FALSE)
+
         echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-    }
-    sqlsrv_free_stmt($getResults);
-?>
+
+        ?>
